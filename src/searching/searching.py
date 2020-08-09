4845f77d65aeee70660c813e54f6b0b403f47bb0
Aaron Williams <aaronrw4@yahoo.com>
2020-08-09 16:06:34 -0700

    Finished searching stretch

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -19,6 +19,49 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-# def agnostic_binary_search(arr, target):
+def agnostic_binary_search(arr, target):
     # Your code here
+    first = 0
+    last = len(arr) - 1
+    start = arr[0]
+    end = arr[last]
+    mid = (first + last) // 2
 
+    if len(arr) == 0:
+        return -1
+    elif start < end:
+        while arr[mid] != target:
+            if first + 1 == last:
+                return -1
+            elif target > arr[mid]:
+                first = mid
+            elif target < arr[mid]:
+                last = mid
+            mid = (first + last) // 2
+        return mid
+    else:
+        while arr[mid] != target:
+            if first + 1 == last:
+                return -1
+            if target < arr[mid]:
+                first = mid
+            elif target > arr[mid]:
+                last = mid
+            mid = (first + last) // 2
+        return mid
+            
+
+    # elif start < end:
+    #     if target > arr[mid]:
+    #         return agnostic_binary_search(arr[mid:], target)
+    #     elif target < arr[mid]:
+    #         return agnostic_binary_search(arr[:mid], target)
+    #     else:
+    #         return mid
+    # else:
+    #     if target > arr[mid]:
+    #         return agnostic_binary_search(arr[:mid], target)
+    #     elif target < arr[mid]:
+    #         return agnostic_binary_search(arr[mid:], target)
+    #     else:
+    #         return mid
